729ca9c26b758c018939f4336ff6d41372b9c6bd
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2025-06-20 02:35:40 +0000

    oo_102

---
+++ oo_102/src/test/java/org/acme/oo_102/Oo102ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.oo_102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oo102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
