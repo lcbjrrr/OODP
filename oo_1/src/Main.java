e45ce1ac9ce8e6a0f44db4a51ec85b6e6dafc811
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2025-08-25 15:37:39 +0000

    Fall 2025

--- oo_1/src/Main.java
+++ oo_1/src/Main.java
@@ -1,20 +1,49 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+//        int x = 1;
+//        System.out.println(x);
+
+//        int x = 1;
+//        if (x==1){
+//            System.out.println("Yes X is 1");
+//        }else{
+//            System.out.println("X is NOT 1");
+//        }
+//
+
+//        for (int i=1;i<=5;i++){
+//            System.out.println(i);
+//        }
+
+//        int y = 5;
+//        while(y>0){
+//            System.out.println(y);
+//            y--;
+//        }
+//
+//        int[] nums = {1,2,3,4,5};
+//        for (int n : nums) {
+//            System.out.println(n);
+//        }
+
+        //int r = add(1,2);
+        //System.out.println(r);
+
         int[] grades = new int[3];
-        grades[0] = 77;
-        grades[1] = 88;
-        grades[2] = 99;
+        grades[0] = 70;
+        grades[1] = 80;
+        grades[2] = 90;
         System.out.println(grades[1]);
         grades[1] = 100;
         System.out.println(grades[1]);
 
+
     }
 
-    static int add(int x, int y) {
-        int sum = x + y;
-        return sum;
-    }
-
+//    static int add(int a, int b) {
+//        return a + b;
+//    }
 }
-
-
