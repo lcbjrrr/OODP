39cf7130799e6b004d2739fe9bead237a5b346f4
Luiz Barboza <62079708+lcbjrrr@users.noreply.github.com>
2025-11-08 03:14:51 +0000

    v_12

---
+++ oo_12/src/main/java/acme/Calculator.java
@@ -0,0 +1,19 @@
+package acme;
+
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double sub(double a, double b) {
+        return a - b;
+    }
+
+    public double mult(double a, double b) {
+        return a * b;
+    }
+
+    public double div(double a, double b) {
+        return a / b;
+    }
+}
